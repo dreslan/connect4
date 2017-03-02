593282e74e634133ed32411b58f51d7a2ac3dae7
tony <dreslan@gmail.com>
2017-03-01 23:12:20 -0500

    Can draw board and detect clicks. Refactored class structure.

--- connect4.js
+++ connect4.js
@@ -1,8 +1,15 @@
 // used to hold and update the state of a connect4 board
 var Board = (function () {
-    function Board() {
-        this.rows = 6;
-        this.cols = 7;
+    function Board(rows, cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.board = [];
+        for (var i = 0; i < rows; i++) {
+            this.board[i] = [];
+            for (var j = 0; j < cols; j++) {
+                this.board[i][j] = new Spot();
+            }
+        }
     }
     Board.prototype.Drop = function (col, player) {
         return false;
@@ -27,7 +34,6 @@ var Board = (function () {
 // a spot on a connect4 board
 var Spot = (function () {
     function Spot() {
-        // all spots start out empty
         this.owner = Player.None;
     }
     return Spot;
@@ -47,46 +53,47 @@ var Mode;
     Mode[Mode["PvAIHard"] = 3] = "PvAIHard";
 })(Mode || (Mode = {}));
 // represents a connect4 webgame
+// manages the ui and the gameplay
+// would like to break this up more
 var Game = (function () {
-    function Game(el) {
-        this.ui = new GameUI(el);
-        this.board = new Board();
-        this.manager = new GameManager(this.board, this.ui);
-    }
-    return Game;
-}());
-// draws and updates the ui
-var GameUI = (function () {
-    function GameUI(el) {
+    function Game(element) {
         this.turn = "it's your turn! Click a column to drop a token.";
-        this.boardElement = el;
-        this.drawBoard();
-    }
-    GameUI.prototype.drawBoard = function () {
-        // todo
-    };
-    GameUI.prototype.drawEmptySpot = function (row, col) {
-    };
-    GameUI.prototype.alertPlayerOfTurn = function (player) {
-        // let the current player know that it's their turn...
-        document.getElementById("status").textContent = Player[player] + " " + this.turn;
-    };
-    return GameUI;
-}());
-// manages game play
-var GameManager = (function () {
-    function GameManager(board, ui) {
-        this.board = board;
-        this.ui = ui;
+        this.boardElement = element;
+        this.board = new Board(6, 7);
         this.mode = Mode.PvP;
         this.currentPlayer = Player.Player1;
+        this.drawBoard();
+        // let the current player know that it's their turn...
+        document.getElementById("status").textContent = Player[this.currentPlayer] + " " + this.turn;
     }
-    // the event handler for a spot that is clicked
-    // also will end up handling game play
-    // should split this out if time permits
-    GameManager.prototype.spotClicked = function (spot) {
+    Game.prototype.drawBoard = function () {
+        for (var i = 0; i < this.board.rows; i++) {
+            for (var j = 0; j < this.board.cols; j++) {
+                var spot = this.drawEmptySpot(i, j);
+                this.boardElement.appendChild(spot);
+                this.board.board[i][j].owner = Player.None;
+            }
+        }
     };
-    return GameManager;
+    Game.prototype.drawEmptySpot = function (row, col) {
+        var _this = this;
+        var spot = document.createElement("div");
+        spot.className = "spot empty";
+        spot.setAttribute("data-row", row.toString());
+        spot.setAttribute("data-col", col.toString());
+        spot.onclick = function () { _this.spotClicked(spot); };
+        spot.style.cssText = "top: " + 100 * row + "px; left: " + 100 * col + "px";
+        this.board.board[row][col].element = spot;
+        return spot;
+    };
+    Game.prototype.spotClicked = function (spot) {
+        if (this.win)
+            return;
+        var row = spot.getAttribute("data-row");
+        var col = spot.getAttribute("data-col");
+        console.log("\"Click at board[" + row + "][" + col + "]\"");
+    };
+    return Game;
 }());
 var GameAINormal = (function () {
     function GameAINormal() {
--- connect4.ts
+++ connect4.ts
@@ -1,16 +1,23 @@
 // used to hold and update the state of a connect4 board
 class Board {
+    public element: HTMLElement;
     public board: Spot[][];
     public rows: number;
     public cols: number;
 
-    constructor() {
-        this.rows = 6;
-        this.cols = 7;
-        
+    constructor(rows: number, cols: number) {
+        this.rows = rows;
+        this.cols = cols;
+        this.board = [];
+        for (let i = 0; i < rows; i++) {
+            this.board[i] = [];
+            for (let j = 0; j < cols; j++) {
+                this.board[i][j] = new Spot();
+            }
+        }
     }
 
-    public Drop(col: number, player: Player) : boolean {
+    public Drop(col: number, player: Player): boolean {
         return false;
     }
 
@@ -37,10 +44,10 @@ class Board {
 
 // a spot on a connect4 board
 class Spot {
+    public element: HTMLElement;
     owner: Player;
 
     constructor() {
-        // all spots start out empty
         this.owner = Player.None;
     }
 }
@@ -60,63 +67,54 @@ enum Mode {
 }
 
 // represents a connect4 webgame
+// manages the ui and the gameplay
+// would like to break this up more
 class Game {
-    ui: GameUI;
-    manager: GameManager;
-    board: Board;
-
-    constructor(el: HTMLElement) {
-        this.ui = new GameUI(el);
-        this.board = new Board();
-        this.manager = new GameManager(this.board, this.ui);
-    }
-}
-
-// draws and updates the ui
-class GameUI {
-    boardElement: HTMLElement;
-    turn: string = "it's your turn! Click a column to drop a token.";
-
-    constructor(el: HTMLElement) {
-        this.boardElement = el;
-        this.drawBoard();
-    }
-
-    drawBoard() {
-        // todo
-    }
-
-    drawEmptySpot(row: number, col: number) {
-        
-    }
-
-    alertPlayerOfTurn(player: Player) {
-        // let the current player know that it's their turn...
-        document.getElementById("status").textContent = Player[player] + " " + this.turn;
-    }
-}
-
-// manages game play
-class GameManager {
-    ui: GameUI;
     board: Board;
     currentPlayer: number;
     win: boolean;
     mode: Mode;
+    boardElement: HTMLElement;
+    turn: string = "it's your turn! Click a column to drop a token.";
 
-    constructor(board: Board, ui: GameUI) {
-        this.board = board;
-        this.ui = ui;
+    constructor(element: HTMLElement) {
+        this.boardElement = element;
+        this.board = new Board(6, 7);
         this.mode = Mode.PvP;
         this.currentPlayer = Player.Player1;
+        this.drawBoard();
 
-        
+        // let the current player know that it's their turn...
+        document.getElementById("status").textContent = Player[this.currentPlayer] + " " + this.turn;
     }
 
-    // the event handler for a spot that is clicked
-    // also will end up handling game play
-    // should split this out if time permits
+    drawBoard() {
+        for (let i = 0; i < this.board.rows; i++) {
+            for (let j = 0; j < this.board.cols; j++) {
+                let spot = this.drawEmptySpot(i, j);
+                this.boardElement.appendChild(spot);
+                this.board.board[i][j].owner = Player.None;
+            }
+        }
+    }
+
+    drawEmptySpot(row: number, col: number) {
+        let spot = document.createElement("div");
+        spot.className = "spot empty";
+        spot.setAttribute("data-row", row.toString());
+        spot.setAttribute("data-col", col.toString());
+        spot.onclick = () => { this.spotClicked(spot) };
+        spot.style.cssText = `top: ${100 * row}px; left: ${100 * col}px`;
+        this.board.board[row][col].element = spot;
+        return spot;
+    }
+
     spotClicked(spot: HTMLElement) {
+        if (this.win) return;
+
+        let row = spot.getAttribute("data-row");
+        let col = spot.getAttribute("data-col");
+        console.log(`"Click at board[${row}][${col}]"`);
     }
 }
 
